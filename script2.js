aa37995b8647e0b9f64f94e3d8364f06e7106807
Onono Okon <ononookon216@gmail.com>
2024-07-10 01:52:20 -0700

    updated the code

--- script2.js
+++ script2.js
@@ -1,32 +1,84 @@
 function getRandomNumber(){
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function getRandomNumber2(){
     return Math.floor(Math.random() * 100) + 1;
 }
 
-let num = getRandomNumber();
+function getRandomNumber3(){
+    return Math.floor(Math.random() * 1000) + 1; 
+}
+
+let num;
+
 
 let guess_number = 0;
 let guess;
 let found = false;
 
-function guessing(){
-while(guess_number<5){
-    prompt("Try guessing the number");
+let difficultyLevel = prompt("Enter the difficulty level you want: easy(1), medium(2) or hard(3)");
 
-    if(guess>num){
-        alert("The number you guessed is larger than the given number");
-    }else if(guess<num){
-        alert("The number is less than the given number, try again");
-    }else if(guess == num){
-        found = true;
-        break;
+function guessing(){
+    if(difficultyLevel == 1){
+        num = getRandomNumber();
+        while(guess_number<2){
+            prompt("Try guessing the number");
+            if(guess>num){
+                alert("The number you guessed is larger than the given number");
+            }else if(guess<num){
+                alert("The number is less than the given number, try again");
+            }else if(guess == num){
+                found = true;
+                break;
+            }
+            guess_number++
+        }
+        if(found == false){
+            alert("Sorry, you have run out of chances, the number is " + num);
+        }else{
+            alert("You guessed right, YAYYYYY!!!");
+        }
+        
+    }else if(difficultyLevel == 2){
+        num = getRandomNumber2();
+        prompt("Try guessing the number");
+        while(guess_number<7){
+            if(guess>num){
+                alert("The number you guessed is larger than the given number");
+            }else if(guess<num){
+                alert("The number is less than the given number, try again");
+            }else if(guess == num){
+                found = true;
+                break;
+            }
+            guess_number++
+        }
+        if(found == false){
+            alert("Sorry, you have run out of chances, the number is " + num);
+        }else{
+            alert("You guessed right, YAYYYYY!!!");
+        }
+    }else if(difficultyLevel == 3){
+        num = getRandomNumber3();
+        prompt("Try guessing the number");
+        while(guess_number<15){
+            if(guess>num){
+                alert("The number you guessed is larger than the given number");
+            }else if(guess<num){
+                alert("The number is less than the given number, try again");
+            }else if(guess == num){
+                found = true;
+                break;
+            }
+            guess_number++
+        }
+        if(found == false){
+            alert("Sorry, you have run out of chances, the number is " + num);
+        }else{
+            alert("You guessed right, YAYYYYY!!!");
+        }
     }
-    guess_number++
-}
-if(found == false){
-    alert("Sorry, you have run out of chances, the number is " + num);
-}else{
-    alert("You guessed right, YAYYYYY!!!");
-}
 }
 
 guessing();
